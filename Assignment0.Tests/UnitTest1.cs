aa8646a0887c2f5499ce5c83cd9b001103bea24a
Cronval <philip.cronval@gmail.com>
2021-09-06 21:11:08 +0200

    add/feature/leapyear

--- Assignment0.Tests/UnitTest1.cs
+++ Assignment0.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace Assignment0.Tests
@@ -6,9 +7,55 @@ namespace Assignment0.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void test_is_2020_LeapYear()
         {
+            //Arrange
+            var input = 2020;
 
+            //Act
+            var output = Program.isLeapyear(input);
+
+            //Assert
+            Assert.Equal(true, output);
+        }
+
+        [Fact]
+        public void test_is_1600_LeapYear()
+        {
+            //Arrange
+            var input = 1600;
+
+            //Act
+            var output = Program.isLeapyear(input);
+
+            //Assert
+            Assert.Equal(true, output);
+        }
+
+        [Fact]
+        public void test_is_2021_LeapYear()
+        {
+            //Arrange
+            var input = 2021;
+
+            //Act
+            var output = Program.isLeapyear(input);
+
+            //Assert
+            Assert.Equal(false, output);
+        }
+
+        [Fact]
+        public void test_is_1900_LeapYear()
+        {
+            //Arrange
+            var input = 1900;
+
+            //Act
+            var output = Program.isLeapyear(input);
+
+            //Assert
+            Assert.Equal(false, output);
         }
     }
 }
